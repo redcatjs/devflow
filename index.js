831a1267e6341806a11f95b08ed2bdcb143dde72
jo <jo@surikat.pro>
2017-08-04 13:57:05 +0200

    CSS Module support for *.module.(css|scss|sass)

--- index.js
+++ index.js
@@ -284,15 +284,16 @@ class devflow {
 							}
 						]
 					},
+					
 					{
-						test: /\.css$/,
+						test: /^(?!.*?\.module).*\.css$/,
 						use: ExtractTextPlugin.extract({
 							fallback: 'style-loader',
 							use: 'css-loader',
 						}),
 					},
 					{
-						test: /\.(sass|scss)$/,
+						test: /^(?!.*?\.module).*\.(sass|scss)$/,
 						use: ExtractTextPlugin.extract({
 							fallback: 'style-loader',
 							use: [
@@ -301,6 +302,37 @@ class devflow {
 							]
 						}),
 					},
+					
+					{
+						test: /^\.module\.css$/,
+						use: ExtractTextPlugin.extract({
+							fallback: 'style-loader',
+							use: [
+								{
+									loader: 'css-loader',
+									options: {
+										modules: true
+									}
+								}
+							],
+						}),
+					},
+					{
+						test: /^\.module\.(sass|scss)$/,
+						use: ExtractTextPlugin.extract({
+							fallback: 'style-loader',
+							use: [
+								{
+									loader: 'css-loader',
+									options: {
+										modules: true
+									}
+								},
+								'sass-loader',
+							]
+						}),
+					},
+					
 					{
 						test: /\.json$/,
 						loader: "json-loader"
